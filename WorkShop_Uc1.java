730763fa1de93922e7bf44bc89111eaad2547296
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-07-15 08:58:18 +0530

    First uscase comitting

---
+++ WorkShop_Uc1.java
@@ -0,0 +1,19 @@
+package day7_Workshop;
+
+public class WorkShop_Uc1
+{
+	public static void main(String[] args) 
+	{
+		int [] arr= {1,2,3,4,5,6,7,8,9};
+	
+		System.out.println("tic tac toe game board");
+		System.out.println("\n");
+
+
+		System.out.println( + arr[0] + " | " + arr[1] + " | " + arr[2]);
+		System.out.println("__________");
+		System.out.println( + arr[3] + " | " + arr[4] + " | " + arr[5]);
+		System.out.println("__________");
+		System.out.println(+ arr[6] + " | " + arr[7] + " | " + arr[8] );
+	}
+}
